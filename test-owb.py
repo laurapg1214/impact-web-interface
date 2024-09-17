7960419f6b4c3eeb556ab54f9045931da66889e8
Laura Purcell-Gates <l.purcellgates@gmail.com>
2024-09-17 11:10:58 +0100

    initial query added; failing tests

--- test-owb.py
+++ test-owb.py
@@ -1,9 +1,22 @@
 import owb_module_base.owb_module.database as db
 
 
-connection = db.create_db_connection(
-    "localhost", 
-    "laurapg1214", 
-    "2B0bjectDataba$3",
-    "owb"
-)
+def main():
+    # create db connection
+    connection = db.create_db_connection(
+        "localhost", 
+        "laurapg1214", 
+        "2B0bjectDataba$3",
+        "owb"
+    )
+
+    show_tables = """
+    SHOW TABLES;
+    """
+
+    execute_query(connection, show_tables)
+
+
+if __name__ == "__main__":
+    main()
+
